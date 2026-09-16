8646a81c11415e1dadff964cf60db44146c483c5
onlyou0933-collab <onlyou0933@gmail.com>
2026-09-16 14:20:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-email = "onlyou0933@gmail.com"
+import os
+import smtplib
+from email.mime.text import MIMEText
 
-print(email)
+EMAIL = os.getenv("EMAIL_ADDRESS")
+PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+msg = MIMEText("GitHub自動寄信測試成功")
+msg["Subject"] = "Stock Scanner Test"
+msg["From"] = EMAIL
+msg["To"] = EMAIL
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(EMAIL, PASSWORD)
+server.send_message(msg)
+server.quit()
+
+print("Email Sent")
