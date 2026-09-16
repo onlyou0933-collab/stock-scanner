a0d3c2e86c1f73dfd28ea76d85a3fbcd2487e9c4
onlyou0933-collab <onlyou0933@gmail.com>
2026-09-16 15:07:17 +0800

    Update main.py
    send email

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("PAGGY TEST")
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+EMAIL = os.getenv("EMAIL_ADDRESS")
+PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+msg = MIMEText("GitHub自動寄信測試成功")
+
+msg["Subject"] = "Stock Scanner Test"
+msg["From"] = EMAIL
+msg["To"] = EMAIL
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+server.login(EMAIL, PASSWORD)
+server.send_message(msg)
+
+server.quit()
+
+print("Email Sent")
